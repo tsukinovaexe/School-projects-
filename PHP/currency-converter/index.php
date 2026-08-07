deedbaf0fdd234de58c57212327e04ff60f9e422
Melany K. <zoeykaasik@gmail.com>
2026-08-07 11:39:23 +0300

    Add currency conversion functionality in index.php

--- PHP/currency-converter/index.php
+++ PHP/currency-converter/index.php
@@ -1,1 +1,20 @@
+<?php
 
+$euro = 100;
+
+$usd = 1.1521;
+$gbp = 0.856718;
+$jpy = 182.548;
+$sek = 10.948;
+
+echo "<h1>Currency Converter</h1>";
+
+echo "Euros: " . $euro . " €<br><br>";
+
+echo "US dollars: " . ($euro * $usd) . " USD<br>";
+echo "British pounds: " . ($euro * $gbp) . " GBP<br>";
+echo "Japanese yen: " . ($euro * $jpy) . " JPY<br>";
+echo "Swedish kronor: " . ($euro * $sek) . " SEK<br>";
+
+
+?>
