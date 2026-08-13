5dd51200031d9b4d31270db169e86afbf5be3526
Melany K. <zoeykaasik@gmail.com>
2026-08-13 12:25:07 +0300

    Add feedback form in index.php

---
+++ PHP/Feedback-Form/index.php
@@ -0,0 +1,17 @@
+<form method="post" action="palaute_postitus.php">
+
+Nimi:<br>
+<input type="text" name="nimi"><br><br>
+
+Sähköposti:<br>
+<input type="text" name="email"><br><br>
+
+Ikä:<br>
+<input type="text" name="ika"><br><br>
+
+Palaute:<br>
+<textarea name="palaute"></textarea><br><br>
+
+<input type="submit" value="Lähetä">
+
+</form>
