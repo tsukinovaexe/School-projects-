f31fcc458978580045b6ff5c31a57a6ba396b0f5
Melany K. <zoeykaasik@gmail.com>
2026-08-18 09:38:28 +0300

    Add second.php for the second page

---
+++ PHP/External-Link-List/second.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Second page</title>
+</head>
+
+<body>
+
+<h1>Second page</h1>
+
+<?php include("links.php"); ?>
+
+</body>
+</html>
