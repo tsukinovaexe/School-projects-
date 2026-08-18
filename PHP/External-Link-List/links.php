5f1f51e1e336725e48a0db0f609c01d3a77c76b9
Melany K. <zoeykaasik@gmail.com>
2026-08-18 09:35:11 +0300

    Create links.php with external links array
    Added an array of external links to the links.php file.

---
+++ PHP/External-Link-List/links.php
@@ -0,0 +1,11 @@
+<?php
+
+$links = [
+    "Google" => "https://www.google.com/",
+    "YouTube" => "https://www.youtube.com/",
+    "Netflix" => "https://www.netflix.com/",
+    "GitHub" => "https://github.com/",
+    "Wikipedia" => "https://www.wikipedia.org/"
+];
+
+?>
